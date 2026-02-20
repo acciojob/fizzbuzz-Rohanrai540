6ed96cd649bc177c9231b787eee63cb23225eaa1
Rohan Rai <92577040+Rohanrai540@users.noreply.github.com>
2026-02-20 19:43:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+let output = "";
+
+for (let i = 1; i <= 100; i++) {
+
+  if (i % 3 === 0 && i % 5 === 0) {
+    output += "FizzBuzz";
+  } 
+  else if (i % 3 === 0) {
+    output += "Fizz";
+  } 
+  else if (i % 5 === 0) {
+    output += "Buzz";
+  } 
+  else {
+    output += i;
+  }
+
+  if (i !== 100) {
+    output += "\n";
+  }
+}
+
+alert(output);
